89e86c61205ea3ed8cac68fae2c3a2ab6bfb9697
Jeroen Tiebout <jeroen@mailbox.org>
2024-02-29 13:52:00 +0100

    update...

--- keyboards/ergodox_ez/keymaps/jeroentbt/keymap.c
+++ keyboards/ergodox_ez/keymaps/jeroentbt/keymap.c
@@ -24,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | ESC    |  TAB |   Q  |   W  |   E  |   R  |  T   |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |   M    | BSPC |   A  |   S  |   D  |   F  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
+ * |   M    | LSft |   A  |   S  |   D  |   F  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
  * |--------+------+------+------+------+------|  B   |           | Meh  |------+------+------+------+------+--------|
  * | LCtrl  | LSft |   Z  |   X  |   C  |   V  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Rrv  |  '"  |      |  TAB | LCtrl|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   | Gui  | LCtl |      |  TAB | LCtrl|                                       |  Up  | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |  R   |  1   |       | Alt  |Ctrl/Esc|
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   KC_EQL,   KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
   KC_ESC,   KC_TAB,      KC_Q,          KC_W,    KC_E,    KC_R,    KC_T,                 TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
-  KC_M  ,   KC_BSPC,     KC_A,          KC_S,    KC_D,    KC_F,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
+  KC_M  ,   KC_LSFT,     KC_A,          KC_S,    KC_D,    KC_F,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
   KC_LCTL,  KC_LSFT,     KC_Z,          KC_X,    KC_C,    KC_V,    KC_B,                 MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT,
-  KC_GRV ,  KC_QUOT,     KC_F1,         KC_TAB,  KC_LCTL,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
+  KC_LGUI,  KC_LCTL,     KC_F1,         KC_TAB,  KC_LCTL,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
                                                                     KC_R, KC_1,               KC_LALT, CTL_T(KC_ESC),
                                                                           KC_3,               KC_PGUP,
                                                             KC_SPC, KC_E, KC_E,                  KC_PGDN, KC_TAB, KC_ENT
